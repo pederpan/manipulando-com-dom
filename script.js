33f3b25f6228fbe9d83ba67e65be1add47ef1c43
alexandre <alexandre.peder@escola.pr.gov.br>
2025-08-07 09:26:41 -0300

    exercicios

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 // ==================================================
 // // ==> SELECIONAR ELEMENTOS <==
 // 1. Selecione o elemento <body> e armazene em uma variável
-let body = 
+let body = document.body
 
 // 2. Selecione todos os elementos <p> e armazene em uma variável
-let paragrafos = 
+let paragrafos = document.getElementsByTagName("p")
 
 // 3. Selecione o elemento com id "meuId" (crie este elemento no HTML se necessário)
-let elementoPorId = 
+let elementoPorId = document.getElementById("meuId")
 
 // ==================================================
 // ==> CRIAR E ADICIONAR ELEMENTOS
 // 4. Crie um novo elemento <h1>
-let novoH1 = 
+let novoH1 = document.createElement("h1")
 
 // 5. Defina o texto do h1 como "Minha Atividade DOM"
-novoH1.textContent = 
+novoH1.textContent = 'Minha Atividade DOM'
 
 // 6. Adicione o h1 ao body
+body.appendChild(novoH1)
 
 // ==================================================
 // ==> MODIFICAR ESTILOS <==
 // 7. Mude a cor de fundo do body para "lightblue"
-body.style.
+body.style.backgroundColor = "lightblue"
 
 // 8. Mude a cor do texto de todos os parágrafos para "darkgray"
 for (let p of paragrafos) {
-  p.style.
+  p.style.color = "darkgray"
 }
 
 // ==================================================
